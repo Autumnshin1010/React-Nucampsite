160e41d6d56f89222f98d1d67ba36931e61f5f3c
Autumn <autumn.1010.shin@gmail.com>
2024-05-31 14:30:36 -0700

    Autumn Sec. Commit - React(Nucamp)

--- src/app/store.js
+++ src/app/store.js
@@ -1,16 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import { campsitesReducer } from '../features/campsites/campsitesSlice';
 import { commentsReducer } from '../features/comments/commentsSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
 import { promotionsReducer } from '../features/promotions/promotionsSlice';
 
-export const store = configureStore({
+
+export const store = configureStore({ //set up a slice of the global state that's named 'campsites'
   reducer: {
     campsites: campsitesReducer,
     comments: commentsReducer,
     partners: partnersReducer,
     promtions: promotionsReducer
-//set up a slice of the global state that's named 'campsites'
-  }
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger])
 });
 
